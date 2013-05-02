26b36750004399c0ed134c766d5b122ff34f6399
M Jehanzeb <mjehanzeb@gmail.com>
2013-05-02 04:30:55 -0400

    some updates

--- imbd_rt_search/moviesearch/tests.py
+++ imbd_rt_search/moviesearch/tests.py
@@ -1,16 +1,13 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
 
 from django.test import TestCase
 
+from moviesearch.models import Movie
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+class MovieMethodTest(TestCase):
+    def test_movie_exist(self):
+    	"""
+   		test_movie_exist should return if movie Titanic already in the database
+    	"""
+    	
+        self.assertEqual(Movie.objects.filter(title='Titanic'), True)
